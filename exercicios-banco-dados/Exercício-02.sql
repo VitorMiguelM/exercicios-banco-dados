82490673f5a7f13e67be3a69d5b88b990615a805
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-06-21 17:24:47 -0300

    att

---
+++ exercicios-banco-dados/Exercício-02.sql
@@ -0,0 +1,42 @@
+﻿DROP TABLE players;
+CREATE TABLE players(
+	estado		CHAR(2),
+	cidade		VARCHAR(140),
+	bairro		VARCHAR(120),
+	cep			CHAR(10),
+	logradouro	VARCHAR(250),
+	numero		SMALLINT,
+	complemento TEXT
+);
+
+INSERT INTO players VALUES ('AC', 'Rio Branco', 'Ayrton Senna', '69.911-866', 'Estrada Deputado José Rui da Silveira Lino', 282, 'casa');
+INSERT INTO players
+(estado, cidade, bairro, cep, logradouro, numero)
+VALUES ('SC', 'Biguaçu', 'Fundos', '88.161-400', 'Rua Júlio Teodoro Martins', 995);
+INSERT INTO players VALUES ('MG', 'Santa Luiza', 'Padre Miguel', '33.082-050', 'Rua Buenos Aires', 371, 'Apartamento');
+INSERT INTO players
+(estado, cidade, bairro, cep, logradouro, numero) 
+VALUES ('BA', 'Salvador', 'São Tomé de Paripe', '40.800-361', 'Travessa Luis Hage', 685);
+INSERT INTO players
+(estado, cidade, bairro, cep, logradouro, numero)
+VALUES('MG', 'Ipatinga', 'Vila Celeste', '35.162-484', 'Rua Antônio Boaventura Batista', 345);
+INSERT INTO players 
+(estado, cidade, bairro, cep, logradouro, numero)
+VALUES ('RS', 'Passo Fundo', 'Nenê Graeff', '99.030-250', 'Rua Itu', 154);
+INSERT INTO players VALUES ('AM', 'Manaus', 'Petrópolis', '69.079-300', 'Rua Coronel Ferreira Sobrinho', 264, 'Fundos');
+INSERT INTO players
+(estado, cidade , bairro, cep, logradouro, numero)
+VAlUES ('TO', 'Gurupi', 'Muniz Santana', '77.402-130', 'Rua Adelmo Aires Negri', 794 );
+INSERT INTO players VALUES ('AC', 'Rio Branco', 'Preventório', '69.900-162', 'Beco da Ligação II', 212, 'Bloco B');
+INSERT INTO players
+(estado, cidade, bairro, cep, logradouro, numero)
+ VALUES ('AP', 'Santana', 'Comercial', '68.925-073', 'Rua Calçoene', 648);
+ INSERT INTO players
+(estado, cidade, bairro, cep, logradouro, numero)
+ VALUES ('PB', 'Cabedelo', 'Camalaú', '58.103-052', 'Rua Siqueira Campos', 249);
+
+
+
+
+
+ SELECT * FROM players;
---
+++ exercicios-banco-dados/Exercício-04.sql
@@ -0,0 +1,26 @@
+﻿DROP TABLE champions;
+CREATE TABLE champions(
+	nome				VARCHAR(100),
+	descricao			VARCHAR(100),
+	habilidade_1		VARCHAR(100),
+	habilidade_2		VARCHAR(100),
+	habilidade_3		VARCHAR(100),
+	habilidade_4		VARCHAR(100),
+	habilidade_5		VARCHAR(100),
+
+);
+
+
+INSERT INTO champions VALUES ('Katarina', 'A lâmina sinistra', 'Voracidade', 'Lâmina Saltitante', 'Preparação', 'Shunpo', 'Lótus da morte');
+INSERT INTO champions VALUES ('Yasuo', 'O imperdoável', 'Esstilo do errante', 'Tempestade de aço', 'Parede de vento', 'Espada Ágil', 'Último suspiro');
+INSERT INTO champions
+(nome, descricao, habilidade_1)
+VALUES ('Master yi', 'O Espadachim Wuju', 'Ataque Duplo');
+INSERT INTO champions VALUES ('Vayne', 'A Caçadora Noturna', 'Caçadora Noturna', 'Rolamento', 'Dardos de Prata', 'Condenar', 'Hora Final');
+INSERT INTO champions VALUES ('Lee Sin', 'O Monge Sego', 'Agitação', 'Onda Sônica/Ataque Ressonante', 'Proteger/Vontade de Ferro', 'Tempestade/Multilar', 'Fúria do Dragão');
+INSERT INTO champions VALUES ('Vi', 'A Defensora De Piltover', 'Blindagem', 'Quebra-Cofres', 'Pancada Certeira', 'Força Exessiva', 'Saque e Enterrada');
+INSERT INTO champions VALUES ('Diana', 'O Escárnio da Lua', 'Espada de Prata Lunar', 'Golpe Crescente', 'Cascata Livida', 'Colapso Minguante', 'Zênite Lunar');
+INSERT INTO champions VALUES ('Annie', 'A Criança Sombria', 'Piromania', 'Desintegrar', 'Incinerar', 'Escudo Fundido', 'Invocar Tibbers');
+INSERT INTO champions VALUES ('Aatrox', 'A Espada Darkin', 'Poço de Sangue', 'Voo Sombrio', 'Sede de Sangue/Preço em Sangue', 'Lâminas da Aflição', 'Massacre');
+
+SELECT * FROM champions;
