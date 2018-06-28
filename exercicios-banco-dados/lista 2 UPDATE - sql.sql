86a6467e7da8b8e906d8252fac739bff60bcf2e6
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-06-28 14:20:14 -0300

    lista 2 update

---
+++ exercicios-banco-dados/lista 2 UPDATE - sql.sql
@@ -0,0 +1,21 @@
+-- UPDATE cidades SET estado = 'SS' WHERE estado = 'SC';
+
+-- UPDATE cidades SET cidade = 'BLUMENAU', estado = 'SC' WHERE cidade = 'BLUMENAU';
+
+-- UPDATE cidades SET cidade = 'BATATA' WHERE cidade LIKE  'BATA%';
+
+-- UPDATE cidades SET cidade = 'LINDO' WHERE cidade LIKE '%BELO%';
+
+-- UPDATE cidades SET estado = 'SC' WHERE cidade LIKE 'INDAIA%';
+
+-- UPDATE cidades SET estado = 'SC' WHERE cidade LIKE '%TIMBÓ%';
+
+-- UPDATE cidades SET  cidade = 'JOSUÉ' WHERE cidade LIKE '%JOSÉ%';
+
+-- UPDATE cidades SET estado = 'PS' WHERE estado LIKE '%SP%';
+
+-- UPDATE cidades SET cidade = 'QUALQUER TEXTO KJKKJJ' WHERE LEN(cidade) = 10;
+
+-- UPTADE cidades SET cidade = 'IT' WHERE cidade LIKE 'IT%';
+
+-- UPDATE cidades SET estado = 'TO' WHERE cidade LIKE '%Ã';
