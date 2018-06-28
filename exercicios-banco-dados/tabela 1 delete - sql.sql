782f809b68b0175291549a2aa6cba60fcd6f53dc
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-06-28 16:28:15 -0300

    tabela 1 delete

---
+++ exercicios-banco-dados/tabela 1 delete - sql.sql
@@ -0,0 +1,42 @@
+-- DELETE FROM pokemons WHERE categoria = 'SEED';
+
+-- DELETE FROM pokemons WHERE nome LIKE 'NID%';
+
+-- DELETE FROM pokemons WHERE categoria LIKE 'SNOW%';
+
+-- DELETE FROM pokemons WHERE ataque = 2 OR defesa = 1;
+
+-- DELETE FROM pokemons WHERE especial_ataque % 2 = 0;
+
+-- DELETE FROM pokemons WHERE LEN(nome) = 10;
+
+-- DELETE FROM pokemons WHERE LEN(categoria) < 4;
+
+-- DELETE FROM pokemons WHERE velocidade % 2 != 0;
+
+-- DELETE FROM pokemons WHERE nome LIKE 'UNO%' OR nome LIKE 'CHARM%';
+
+-- DELETE FROM pokemons WHERE categoria = 'FLOWER' AND codigo > 45 OR codigo < 200;
+
+-- DELETE FROM pokemons WHERE descricao LIKE '%SHELL%';
+
+-- DELETE FROM pokemons WHERE peso >= 100;
+
+-- DELETE FROM pokemons WHERE altura < 1;
+
+-- DELETE FROM pokemons WHERE especial_defesa > 3;
+
+-- DELETE FROM pokemons WHERE LEN(descricao) > 150; (X)
+
+-- DELETE FROM pokemons WHERE descricao LIKE '%GOOD%'; (X)
+
+-- DELETE FROM pokemons WHERE codigo % 2 = 0;
+
+-- DELETE FROM pokemons WHERE nome = 'LILEEP'; (X)
+
+-- DELETE FROM pokemons WHERE especial_ataque = 5;
+
+-- DELETE FROM pokemons WHERE codigo < 100; (X)
+
+
+
