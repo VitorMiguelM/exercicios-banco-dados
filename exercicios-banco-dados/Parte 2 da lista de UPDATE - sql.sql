8d9a41053efe0b4f5e1bbd34be84b9fc804a305d
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-06-28 14:20:32 -0300

    parte 2 da lista 1 de update

---
+++ exercicios-banco-dados/Parte 2 da lista de UPDATE - sql.sql
@@ -0,0 +1,9 @@
+-- UPDATE pokemons SET categoria = 'WATER' WHERE descricao LIKE '%FLAMES%';
+
+-- UPDATE pokemons SET codigo = 151 WHERE codigo = 155;
+
+-- UPDATE pokemons SET nome = 'NARUTO', ataque = 1 WHERE nome = 'KABUTO';
+
+--UPDATE pokemons SET nome = 'SASUKE', especial_ataque = 8002, ataque = 8001 WHERE nome = 'MEW' OR nome = 'MEWTWO';
+
+-- UPDATE pokemons SET descricao = 'LOREM IPSUM', nome = 'TYRANITAR', categoria = 'WOOD GECKO' WHERE codigo % 2 = 0;
