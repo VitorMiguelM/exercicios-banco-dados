a08e05f176d9aa1a720aa45ed216984bf618fd13
Vitor Miguel <38699921+VitorMiguelM@users.noreply.github.com>
2018-06-28 16:55:08 -0300

    tabela 2 DELETE

---
+++ exercicios-banco-dados/tabela 2 DELETE - sql.sql
@@ -0,0 +1,11 @@
+-- DELETE FROM cidades WHERE estado = 'RS';
+
+-- DELETE FROM cidades WHERE estado = 'AC' AND cidade LIKE 'R%';
+
+-- DELETE FROM cidades WHERE cidade LIKE '%GOAS';
+
+-- DELETE FROM cidades WHERE cidade LIKE '%IRMÃOS%';
+
+-- DELETE FROM cidades WHERE estado = 'MG';
+
+-- DELETE FROM cidades WHERE cidade = 'DOURADINA';
